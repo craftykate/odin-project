f6bd050068a37aef78580f466ae6bc9bf6d3fff6
Kate McFaul <katemcfaul@gmail.com>
2014-12-06 16:30:39 -0800

    Add images to blog

--- ch02_rails_blog/app/controllers/articles_controller.rb
+++ ch02_rails_blog/app/controllers/articles_controller.rb
@@ -47,6 +47,6 @@ class ArticlesController < ApplicationController
 	private
 
 	def article_params
-		params.require(:article).permit(:title, :body, :tag_list)
+		params.require(:article).permit(:title, :body, :tag_list, :image)
 	end
 end
--- ch02_rails_blog/app/models/article.rb
+++ ch02_rails_blog/app/models/article.rb
@@ -2,6 +2,8 @@ class Article < ActiveRecord::Base
 	has_many :comments
 	has_many :taggings
 	has_many :tags, through: :taggings
+	has_attached_file :image
+	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 
 	def tag_list
 		tags.join(", ")
