a404f978d157d4e58b68ea53eb03b9b075a7e3a8
Kate McFaul <katemcfaul@gmail.com>
2014-12-11 18:14:17 -0800

    Add forms project

--- Chapter_04-Advanced_Rails/forms/config/routes.rb
+++ Chapter_04-Advanced_Rails/forms/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  resources :users, only: [:new, :create, :edit, :update]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
