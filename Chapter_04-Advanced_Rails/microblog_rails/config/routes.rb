0e93d5f37e3c29593ed1f700a513127c8348e07b
Kate McFaul <katemcfaul@gmail.com>
2014-12-11 20:31:32 -0800

    Finish user signup

--- Chapter_04-Advanced_Rails/microblog_rails/config/routes.rb
+++ Chapter_04-Advanced_Rails/microblog_rails/config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get 'contact' => 'static_pages#contact'
   get 'signup'  => 'users#new'
 
+  resources :users
+
 
 
   # The priority is based upon order of creation: first created -> highest priority.
