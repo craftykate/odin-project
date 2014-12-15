12a708a6997e801302a0effcd37d06f4a4655eca
Kate McFaul <katemcfaul@gmail.com>
2014-12-15 09:41:12 -0800

    Add remembering users

--- Chapter_04-Advanced_Rails/members-only-real/app/controllers/sessions_controller.rb
+++ Chapter_04-Advanced_Rails/members-only-real/app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
 		user = User.find_by(email: params[:session][:email].downcase)
 		if user && user.authenticate(params[:session][:password])
 			log_in user
+			remember user
 			redirect_to root_path
 		else
 			flash.now[:error] = "Invalid email/password combination"
@@ -15,7 +16,7 @@ class SessionsController < ApplicationController
 	end
 
 	def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
 	end
 end
--- Chapter_04-Advanced_Rails/members-only-real/app/models/user.rb
+++ Chapter_04-Advanced_Rails/members-only-real/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+	attr_accessor :remember_token
+
 	before_save { self.email = email.downcase }
 	validates :username, 
 						presence: true, 
@@ -13,4 +15,27 @@ class User < ActiveRecord::Base
 	validates :password, 
 						length: { minimum: 6 },
             allow_blank: true
+
+  def User.digest(string)
+  	cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+  	BCrypt::Password.create(string, cost: cost)
+  end
+
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+	def forget
+		update_attribute(:remember_digest, nil)
+	end
+
 end
